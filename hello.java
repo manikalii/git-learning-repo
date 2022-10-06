eec1d9fbdfe36eb7e581601fca55d5622b0e20ff
manikali <manikalixyz123@gmail.com>
2022-10-06 22:24:08 +0530

    new method added to Hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,11 @@ public class Hello{
     public static void main(String[] args)
     {
         System.out.println("vs code is awesome");
+        grettings("Manik ali");
+    }
+
+    public static void grettings(String name)
+    {
+        System.out.println("welcome" + name)
     }
 }
